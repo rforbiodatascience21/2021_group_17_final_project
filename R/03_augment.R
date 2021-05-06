19a1709ccb616833a09c9ffaf4de5dd487af191d
carcarmor <clotisccm@gmail.com>
2021-05-06 08:08:59 +0000

    Attributes added

--- R/03_augment.R
+++ R/03_augment.R
@@ -11,13 +11,17 @@ source(file = "R/99_project_functions.R")
 
 
 # Load data ---------------------------------------------------------------
-my_data_clean <- read_tsv(file = "data/02_my_data_clean.tsv")
+data_clean <- read_tsv(file = "data/02_data_clean.tsv")
 
 
 # Wrangle data ------------------------------------------------------------
-my_data_clean_aug <- my_data_clean # %>% ...
+data_clean_aug <- data_clean %>% mutate(Casesper100kpp = round((Cases / Population * 100000), digits = 2)) %>% 
+  mutate(Deathsper100kpp = round((Deaths / Population * 100000), digits = 2)) %>%
+  mutate(FatalityRate = round((Deaths / Cases * 100), digits = 2)) %>%
+  mutate(PopDens)
+
 
 
 # Write data --------------------------------------------------------------
-write_tsv(x = my_data_clean_aug,
-          file = "data/03_my_data_clean_aug.tsv")
+write_tsv(x = data_clean_aug,
+          file = "data/03_data_clean_aug.tsv")
